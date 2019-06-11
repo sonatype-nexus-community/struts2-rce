d8e9e8460e7ad9f9a3caccecfad195449b7e720b
ajurgenson55 <ajurgenson@sonatype.com>
2019-06-11 12:39:51 -0400

    added repository tag to move components

--- webgoat-pipeline.groovy
+++ webgoat-pipeline.groovy
@@ -109,7 +109,7 @@ pipeline
     }
     post ('Promotion') {
         always {
-            moveComponents destination: 'maven-dev', nexusInstanceId: 'nxrm3', tagName: 'passed-dev-scan'
+            moveComponents destination: 'maven-dev', nexusInstanceId: 'nxrm3', tagName: 'passed-dev-scan', repository: 'maven-releases'
             echo 'Successfully promoted component'
         }
     }
